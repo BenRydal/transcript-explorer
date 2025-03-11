721e70478f01714834580f39815ad67d1dcbe38c
Edwin Zhao <contact@edwinzhao.com>
2025-03-11 12:39:07 -0700

    fix: addition of users fix when scrolling (#12)
    * fix: addition of users fix when scrolling
    
    * color text in user buttons
    
    ---------
    
    Co-authored-by: BenRydal <benrydal@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		replace({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+		})
+	]
 });
